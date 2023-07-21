39d051a1bc57c8a7a629b04cebe448e27b99bb63
fz-hd-khn <khanfaizahuda@gmail.com>
2023-07-20 21:49:57 -0400

    added jsx general info content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dashboard-content">
+    <div className="information">
+      <div className="general-info-content">
+        <u>Info </u>
+        <span className="info">Health Card # : XXXXXXXXXX</span>
+        <span className="info">Height: XXXXX</span>
+        <span className="info">Weight: XXXXX</span>
+        <span className="info">Health Conditions: XXXXX</span>
+        <span className="info">Medications: XXXXX</span>
+        <span className="info">
+          Family Doctor: Location, Phone Number &amp; Practice #
+        </span>
+      </div>
+      <div className="calendar">
+        <u>Calendar</u>
+        <span className="visits-header">
+          <b>Past Visits</b>
+          <span>December 31, 2022 (Dr. Jones- Cardiology) </span>
+          <span> 30, 2023 (Dr.Smith, Ultrasound)</span>
+        </span>
+        <span className="visits-header">
+          <b>Future Visits</b>
+          <span>March 12,2023 (Family Doctor, General)</span>
+        </span>
+      </div>
     </div>
+    <div className="quick-access">
+      <u>Quick Access</u>
+    </div>
+  </div>
   );
 }
 
